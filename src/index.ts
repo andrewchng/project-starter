4605c0694c91e7476c9e130afac84762f868cebc
Andrew Chng <andrewchng@t410>
2025-05-10 17:49:27 +0800

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import inquirer from "inquirer"
 
 async function initProject() {
-        console.log("Init Project")
-        const answers = inquirer.prompt([
-                {
-                        type: 'confirm',
-                        name: 'createTypescriptProject',
-                        default: false,
-                        message: "Create a Typescript Project?"
-                }
-        ])
+    console.log("Init Project")
+    const answers = inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'createTypescriptProject',
+            default: false,
+            message: "Create a Typescript Project?"
+        }
+    ])
 }
 
 initProject().catch((error) => {
-        console.log(error)
+    console.log(error)
 })
