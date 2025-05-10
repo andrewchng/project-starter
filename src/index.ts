9b933531e0c1de0a8fcafd347a197385ddc5c477
Andrew Chng <andrewchng@t410>
2025-05-10 18:33:15 +0800

    updates

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,21 @@ async function initProject() {
     const answers = inquirer.prompt([
         {
             type: 'confirm',
-            name: 'createTypescriptProject',
+            name: 'createTypescript',
             default: false,
             message: "Create a Typescript Project?"
-        }
-    ])
-}
+        },
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'Project name:',
+            default: 'typescript-app',
+            validate: input => /^[a-zA-Z0-9-_]+$/.test(input) || 'Project name must be alphanumeric with hyphens or underscores',
+            when: answers => answers.createTypescript,
+        }]
+    );
 
+}
 initProject().catch((error) => {
     console.log(error)
 })
