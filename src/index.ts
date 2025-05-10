13b581ff0d6008c6192a4f536848fe7ee40c0d62
Andrew Chng <andrewchng@t410>
2025-05-10 17:25:21 +0800

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world");
+import inquirer from "inquirer"
+
+async function initProject() {
+        console.log("Init Project")
+        const answers = inquirer.prompt([
+                {
+                        type: 'confirm',
+                        name: 'createTypescriptProject',
+                        default: false,
+                        message: "Create a Typescript Project?"
+                }
+        ])
+}
+
+initProject().catch((error) => {
+        console.log(error)
+})
